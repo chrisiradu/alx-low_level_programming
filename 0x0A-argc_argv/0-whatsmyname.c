8a67d8f979d892ae4fc78172df2d3d12002a6bfa
chrisiradu <christianiradukunda200@gmail.com>
2023-03-10 12:22:57 -0800

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - a program that prints its name, followed by a new line
@@ -6,8 +7,10 @@
  * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
-int main(int argc  __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
 	return (0);
 }
