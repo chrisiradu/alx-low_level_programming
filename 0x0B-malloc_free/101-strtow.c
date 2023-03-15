98219f13200d22d6a3e300795c4af0e849ce0065
chrisiradu <christianiradukunda200@gmail.com>
2023-03-14 17:19:13 -0700

    my commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -71,15 +71,20 @@ end = i;
 tmp = (char *) malloc(sizeof(char) * (c + 1));
 
 if (tmp == NULL)
-				return (NULL);
-				while (start < end)
+
+	return (NULL);
+
+while (start < end)
+{
 				*tmp++ = str[start++];
 				*tmp = '\0';
 				array[k] = tmp - c;
 				k++;
-			c = 0;
-			}
-		}
+		c = 0;
+}
+			
+}
+}
 	else if (c++ == 0)
 	start = i;
 
