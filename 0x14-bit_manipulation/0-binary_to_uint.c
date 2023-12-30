0ef59f7d222cbb550274a11a68090ebae2556394
chrisiradu <christianiradukunda200@gmail.com>
2023-12-30 09:54:44 -0800

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,8 +21,6 @@ unsigned int binary_to_uint(const char *b)
 	{
 	if (b[len] != '0' && b[len] != '1')
 	return (0);
-	sum += (b[len] != '0' && b[len] != '1')
-	return (0);
 	sum += (b[len] - '0') * mult;
 	mult *= 2;
 	}
